8fd4b4d55289abf59205ef566bec682667e00d9d
jackjavi <jackmtembete@gmail.com>
2022-12-01 10:10:58 -0800

    Fixed error

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,12 +10,12 @@ if __name__ == "__main__":
     b = int(sys.argv[3])
 
     if (len(sys.argv)) < 4:
-        print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
-        exit(1)
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>".format(sys.argv[0]))
+        sys.exit(1)
 
     if operator != ('+' or '-' or '/' or '*'):
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
 
     if operator == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
