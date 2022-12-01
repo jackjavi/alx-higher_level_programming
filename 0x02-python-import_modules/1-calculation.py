167cdd04303c3a4069641bdd409f4f98a82ba52b
jackjavi <jackmtembete@gmail.com>
2022-12-01 00:47:15 -0800

    calculator

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 1-calculation.py
+# Jack Mtembete
+
+if __name__ == "__main__":
+    """Multiplication, Division, Addition, Subtraction"""
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
