bbdfcb01f41be4e3106c9bc01cea4f8cb7af7796
jackjavi <jackmtembete@gmail.com>
2022-12-05 02:50:58 -0800

    Fixed error

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,16 +2,7 @@
 
 def print_reversed_list_integer(my_list=[]):
     """Reverses elements in a list"""
-    length = 0
-    for i in my_list:
-        length += 1
-    i = 0
-    while (i < (length)):
-        temp = my_list[i]
-        my_list[i] = my_list[length - 1]
-        my_list[length - 1] = temp
-        i += 1
-        length -= 1
-
-    for i in range(len(my_list)):
-        print("{}".format(my_list[i]))
+    if isinstance(my_list, list):
+        my_list.reverse()
+        for i in my_list:
+            print("{:d}".format(i))
