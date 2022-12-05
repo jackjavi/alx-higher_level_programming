6fd26cfd5bd3f4d1df37d6108dd47555f4da0d1b
jackjavi <jackmtembete@gmail.com>
2022-12-04 23:49:02 -0800

    Fixed error

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 
 def replace_in_list(my_list, idx, element):
     """Replace an element of a list at a specific position"""
-    if (idx < 0) or (((len(my_list)) - 1) > idx):
+    if (idx < 0) or (((len(my_list)) - 1) < idx):
         return (my_list)
     else:
         my_list[idx] = element
