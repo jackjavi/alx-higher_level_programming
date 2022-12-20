5638dc348ea211f994930b59094afa7c1babca04
jackjavi <jackmtembete@gmail.com>
2022-12-20 09:06:46 -0800

    Define a square, find area and print the square to stdout

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Define a square class"""
+
+
+class Square:
+    def __init__(self, size=0):
+        """
+        Initialize the square class.
+
+        Args:
+            size: size of suare.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Prints instdout the current square area
+        with the character #
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
