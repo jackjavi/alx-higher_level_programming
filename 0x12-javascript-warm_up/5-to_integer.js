8d856fac871cc4c3dc6fee225d4cac3e148f8bb1
jackjavi <jackmtembete@gmail.com>
2023-03-13 21:25:26 -0700

    Fixed error.

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,2 +1,3 @@
 #!/usr/bin/node
-console.log(`My number: ${Math.floor(process.argv[2])? Math.floor(process.argv[2]) : "Not a number"}`);
+const num = Math.floor(Number(process.argv[2]));
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
