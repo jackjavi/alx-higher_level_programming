a6e137abdf693d7d40a42ce020f8baa64c0c4338
jackjavi <jackmtembete@gmail.com>
2023-04-25 11:43:45 +0000

    Staus code.

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-
 const request = require('request');
-const url = process.argv[2];
 request.get(process.argv[2]).on('response', function (response) {
-	console.log(`code: ${response.statusCode}`);
+  console.log(`code: ${response.statusCode}`);
 });
